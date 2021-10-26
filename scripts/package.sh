78992c9916236e49697673362cfc9c63cebe39b3
Huo Linhe <linhehuo@gmail.com>
2021-10-26 14:00:27 +0800

    [TD-10774]<fix>: add package script

---
+++ scripts/package.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+name=`jq '.id + "-v" + .info.version' -r src/plugin.json`
+echo generate plugin as $name
+rm -rf $name
+cp -r dist $name
+zip -r $name.zip $name
+echo packaged to $name.zip
