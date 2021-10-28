4bd60f9b188bd665a92f54c43f4a5a8edbf94ddc
afwerar <1296468573@qq.com>
2021-10-28 10:19:14 +0800

    [TD-10760]<fix>:could not locate user error.

--- src/config_ctrl.js
+++ src/config_ctrl.js
@@ -1,14 +1,12 @@
 export class GenericDatasourceConfigCtrl {
     /** @ngInject */
     constructor($scope, $injector, $q) {
-        console.log(this);
         if(!this.current.jsonData.user) {
             this.current.jsonData.user="root";
         }
         if(!this.current.jsonData.password) {
             this.current.jsonData.password="taosdata";
         }
-        console.log(this);
     }
   }
 
