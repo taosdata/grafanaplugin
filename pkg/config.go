f2e4f246aa24494303be8e24994a5f2c3d89490c
Huo Linhe <linhehuo@gmail.com>
2022-05-25 01:06:52 +0800

    chore: remove unused code

--- pkg/config.go
+++ pkg/config.go
@@ -4,25 +4,6 @@ import (
 	"strings"
 )
 
-// type SmsConfInfo struct {
-// 	AccessKeyId      string   `json:"aliSmsAccessKeyId"`
-// 	AccessKeySecret  string   `json:"aliSmsAccessKeySecret"`
-// 	SignName         string   `json:"aliSmsSignName"`
-// 	TemplateCode     string   `json:"aliSmsTemplateCode"`
-// 	TemplateParam    string   `json:"aliSmsTemplateParam"`
-// 	PhoneNumbers     []string `json:"aliSmsPhoneNumbers"`
-// 	PhoneNumbersList string   `json:"aliSmsPhoneNumbersList"`
-// 	ListenAddr       string   `json:"aliSmsListenAddr"`
-// }
-
-// func (sc *SmsConfInfo) GetPhoneNumbers() []string {
-// 	if len(sc.PhoneNumbersList) != 0 {
-// 		return strings.Split(sc.PhoneNumbersList, ",")
-// 	} else {
-// 		return sc.PhoneNumbers
-// 	}
-// }
-
 type JsonData struct {
 	User     string `json:"user"`
 	Password string `json:"password"`
