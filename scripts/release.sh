f76da14c903305c46592eb7f69106102e2770bb3
Huo Linhe <linhehuo@gmail.com>
2022-06-11 17:32:24 +0800

    release: v3.2.6

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,7 +7,7 @@ if [ "$newv" = "" ]; then
 fi
 sed -Ei 's#"version":\s*.*$#"version": "'$newv'"#' package.json src/plugin.json
 sed -Ei 's#"updated":\s*.*$#"updated": "'`date +%F`'"#' src/plugin.json
-git cliff -t v$newv |tee CHANGELOG.md
+git cliff -t v$newv |tee CHANGELOG.md > /dev/null
 
 git config user.email "github-actions@github.com"
 git config user.name "GitHub Actions [bot]"
@@ -17,4 +17,4 @@ git commit -a -m "chore(release): bump v$newv"
 git push
 
 git tag v$newv
-git push origin v$newv:$newv --force
+git push origin v$newv:v$newv --force
