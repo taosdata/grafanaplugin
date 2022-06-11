b1765def41269feca0028a373589fadfc71ee6ef
Huo Linhe <linhehuo@gmail.com>
2022-06-11 16:59:25 +0800

    enh: anyone can request a release by pr
    Just by title the pr with:
    
    ```
    chore(release): v<version>
    ```

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,3 +9,9 @@ sed -Ei 's#"version":\s*.*$#"version": "'$newv'"#' package.json src/plugin.json
 sed -Ei 's#"updated":\s*.*$#"updated": "'`date +%F`'"#' src/plugin.json
 git cliff -t v$newv |tee CHANGELOG.md
 
+git commit -a -m "chore(release): bump v$newv"
+
+git push
+
+git tag v$newv
+git push origin v$newv:$newv --force
